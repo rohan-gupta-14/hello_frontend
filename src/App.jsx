036189e63cd05090ad349ed41862d5e94c63b15d
rohan-gupta-14 <rohangupta.xiv@gmail.com>
2026-02-25 21:35:41 +0530

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import { useState, useEffect } from 'react'
 
 function App() {
-  const [message, setMessage] = useState()
-
-  useEffect(() => {
-    fetch('http://localhost:3001/api/hello')
-      .then(res => res.json())
-      .then(data => setMessage(data.message))
-      .catch(() => setMessage('Failed to connect to backend'))
-  }, [])
+ 
 
   return (
     <div>
       <h1>Hii, Rohan this side :)</h1>
-      <p>{message}</p>
     </div>
   )
 }
